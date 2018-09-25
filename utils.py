dde140b672ea220715cdafcb88248ec25f741f56
pblouw <pblouw@uwaterloo.ca>
2018-09-25 13:13:56 -0400

    Add tutorial content

---
+++ utils.py
@@ -0,0 +1,60 @@
+import string
+
+import numpy as np
+import tensorflow as tf
+
+allowed_text = ["loha", "alha", "aloa", "aloh", "aoha", "aloha"]
+id_to_char = np.array([x for x in string.ascii_lowercase + "\" -|"])
+
+
+def ce_loss(x, y, weight=1):
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits=x, labels=y)
+    return weight * tf.reduce_sum(loss)
+
+
+def weight_init(shape):
+    '''Convenience function for randomly initializing weights'''
+    weights = np.random.uniform(-0.05, 0.05, size=shape)
+    return weights
+
+
+def merge(chars):
+    """Merge repeated characters and strip blank CTC symbol"""
+    acc = ["-"]
+    for c in chars:
+        if c != acc[-1]:
+            acc.append(c)
+
+    acc = [c for c in acc if c != "-"]
+    return "".join(acc)
+
+
+def predict_text(sim, probe, n_steps, p_time=10):
+    '''Predict a text transcription from the current simulation state'''
+    n_frames = int(n_steps / p_time)
+    char_data = sim.data[probe]
+    # print(char_data.shape)
+    n_chars = char_data.shape[1]
+
+    # reshape to seperate out each window frame that was presented
+    char_out = np.reshape(char_data, (n_frames, p_time, n_chars))
+    # print("Out: %s, %s" % (char_out.min(), char_out.max()))
+
+    # take most ofter predicted char over each frame presentation interval
+    char_ids = np.argmax(char_out, axis=2)
+    char_ids = [np.argmax(np.bincount(i)) for i in char_ids]
+
+    text = merge(''.join([id_to_char[i] for i in char_ids]))
+    text = merge(text)  # merge repeats to help autocorrect
+
+    return text
+
+
+def create_stream(stream, dt=0.001):
+
+    def play_stream(t, stream=stream):
+
+        ti = int(t / dt)
+        return stream[ti % len(stream)]
+
+    return play_stream
